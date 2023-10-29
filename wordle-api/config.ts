74aa7a642509e8893ed32ffceeefcc38c04ca84e
Harry <hazzarius@gmail.com>
2023-10-29 21:13:59 +0000

    added function to start prod

--- wordle-api/config.ts
+++ wordle-api/config.ts
@@ -8,19 +8,19 @@ export default {
     sql: {
         host: process.env.POSTGRES_HOST || 'localhost',
         database: 'wordle-online',
-        port: parseInt(process.env.POSTGRES_PORT) || 5432,
+        port: process.env.POSTGRES_PORT || 5432,
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASS,
     },
     server: {
         host: process.env.SERVER_HOST || 'http://localhost:8080',
-        port: parseInt(process.env.SERVER_PORT) || 8080,
-    },
-    websocket: {
-        port: parseInt(process.env.WS_PORT) || 8081
+        port: process.env.SERVER_PORT || 8080,
     },
     client: {
         host: process.env.CLIENT_HOST || 'http://localhost:3000',
-        port: parseInt(process.env.CLIENT_PORT) || 3000,
+        port: process.env.CLIENT_PORT || 3000,
     },
+    websocket: {
+        port: 8081
+    }
 };
