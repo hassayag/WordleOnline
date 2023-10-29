c9105520e6d014b662239edcf2330381c503b317
Harry <hazzarius@gmail.com>
2023-10-29 22:47:51 +0000

    fixed create.sql path

--- wordle-api/src/db/init.ts
+++ wordle-api/src/db/init.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import psql from './db';
 
 const create = async () => {
-    const filePath = path.resolve('../../data/create.sql');
+    const filePath = path.resolve('./data/create.sql');
     const createFile = fs.readFileSync(filePath).toString();
     console.info('Creating DB');
     try {
