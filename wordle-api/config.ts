689ff5738d81258ed25ea386724eeed3b9a2d68b
Harry <hazzarius@gmail.com>
2023-11-03 21:23:29 +0000

    enable ssl on postgres client

--- wordle-api/config.ts
+++ wordle-api/config.ts
@@ -11,6 +11,7 @@ export default {
         port: process.env.POSTGRES_PORT || 5432,
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASS,
+        ssl: true
     },
     server: {
         host: process.env.SERVER_HOST || 'http://localhost:8080',
