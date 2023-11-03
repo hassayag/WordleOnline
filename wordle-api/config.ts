238c2f298d05f3faef104b708b3c8576f7cc2ee5
Harry <hazzarius@gmail.com>
2023-11-03 21:58:03 +0000

    disable ssl

--- wordle-api/config.ts
+++ wordle-api/config.ts
@@ -3,23 +3,19 @@
 import { config } from 'dotenv';
 
 config();
-const {POSTGRES_HOST: host, POSTGRES_DATABASE: database, POSTGRES_PORT: port, POSTGRES_USER: user, POSTGRES_PASS: password} = process.env
 
-let sqlConfig;
-if (process.env.ENVIRONMENT === 'development') {
-    sqlConfig = {
-        host: process.env.POSTGRES_HOST || 'localhost',
-        database: process.env.POSTGRES_DATABASE || 'postgres',
-        port: process.env.POSTGRES_PORT || 5432,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASS,
+const sqlConfig = {
+    host: process.env.POSTGRES_HOST || 'localhost',
+    database: process.env.POSTGRES_DATABASE || 'postgres',
+    port: process.env.POSTGRES_PORT || 5432,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    ssl: {
+        rejectUnauthorized: false
     }
 }
-else {
-    sqlConfig = {
-        connectionString: `postgres://${user}:${password}@${host}:${port}/${database}?sslmode=require`
-    }
-}
+
+console.log(sqlConfig)
 
 export default {
     sql: sqlConfig,
