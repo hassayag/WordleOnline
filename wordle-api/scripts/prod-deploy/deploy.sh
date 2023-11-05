08c54360a7cbf3c4f6f8c060e99a4463ac9ced77
Harry <hazzarius@gmail.com>
2023-11-05 17:49:42 +0000

    fix deploy script killing app on close

--- wordle-api/scripts/prod-deploy/deploy.sh
+++ wordle-api/scripts/prod-deploy/deploy.sh
@@ -1,9 +1,10 @@
-ssh -i "ssh-key.pem" ec2-user@$WORDLE_API_EC2_URL << EOF
+ssh -i "ssh-key.pem" ec2-user@$WORDLE_API_EC2_URL<< EOF
     cd WordleOnline/wordle-api
     sudo pkill -f node
     git checkout master
     git pull
     npm ci
     npm run build
-    npm run start.prod &
+    nohup npm run start.prod > app.log 2>&1 &
+    disown
 EOF
